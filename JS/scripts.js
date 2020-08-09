69282e78d26b3df3bbf368806f3e6e494d30ceda
virsail <virsaileric@gmail.com>
2020-08-10 00:05:46 +0300

    change the index.html file name

--- JS/scripts.js
+++
@@ -1,9 +0,0 @@
-var convertBtn = document.querySelector('.convert-button');
-var URLinput = document.querySelector('.URL-input');
-convertBtn.addEventListener('click', () => {
-    console.log(`URL: ${URLinput.value}`);
-    sendURL(URLinput.value);
-});
-function sendURL(URL) {
-    // We will put code here later
-}
