2a73b7c3a9411d2eb088aa463be6d5bec1d45d3b
virsail <virsaileric@gmail.com>
2020-08-09 22:39:36 +0300

    add a redirect url to the server

--- Js/script.js
+++ Js/script.js
@@ -20,3 +20,12 @@ function sendURL(URL) {
     }).then(res => res.json())
     .then(json => console.log(json));
 }
+var convertBtn = document.querySelector('.convert-button');
+var URLinput = document.querySelector('.URL-input');
+convertBtn.addEventListener('click', () => {
+    console.log(`URL: ${URLinput.value}`);
+    sendURL(URLinput.value);
+});
+function sendURL(URL) {
+    window.location.href = `http://localhost:4000/download?URL=${URL}`;
+}
