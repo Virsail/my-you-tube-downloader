5e07e719ceaac3b726ec892f2f78db11331c962f
virsail <virsaileric@gmail.com>
2020-08-09 23:28:27 +0300

    add a function to update the version of lock.jsons

--- Server/index.js
+++ Server/index.js
@@ -2,33 +2,54 @@ const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
-app.listen(4000, () => {
-    console.log('Server Works !!! At port 4000');
-});
-const express = require('express');
-const cors = require('cors');
-const ytdl = require('ytdl-core');
-const app = express();
+const PORT = 4000;
+
 app.use(cors());
-app.listen(4000, () => {
-    console.log('Server Works !!! At port 4000');
+
+app.listen(PORT, () => {
+	console.log(`Server Works !!! At port ${PORT}`);
 });
-app.get('/download', (req,res) => {
-    var URL = req.query.URL;
-    res.json({url:URL});
-})
-const express = require('express');
-const cors = require('cors');
-const ytdl = require('ytdl-core');
-const app = express();
-app.use(cors());
-app.listen(4000, () => {
-    console.log('Server Works !!! At port 4000');
+
+app.get('/downloadmp3', async (req, res, next) => {
+	try {
+		var url = req.query.url;
+		let title = 'audio';
+
+		await ytdl.getBasicInfo(url, {
+			format: 'mp4'
+		}, (err, info) => {
+			if (err) throw err;
+			title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
+		});
+
+		res.header('Content-Disposition', `attachment; filename="${title}.mp3"`);
+		ytdl(url, {
+			format: 'mp3',
+			filter: 'audioonly',
+		}).pipe(res);
+
+	} catch (err) {
+		console.error(err);
+	}
 });
-app.get('/download', (req,res) => {
-var URL = req.query.URL;
-res.header('Content-Disposition', 'attachment; filename="video.mp4"');
-ytdl(URL, {
-    format: 'mp4'
-    }).pipe(res);
+
+app.get('/downloadmp4', async (req, res, next) => {
+	try {
+		let URL = req.query.url;
+		let title = 'video';
+
+		await ytdl.getBasicInfo(URL, {
+			format: 'mp4'
+		}, (err, info) => {
+			title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
+		});
+
+		res.header('Content-Disposition', `attachment; filename="${title}.mp4"`);
+		ytdl(URL, {
+			format: 'mp4',
+		}).pipe(res);
+
+	} catch (err) {
+		console.error(err);
+	}
 });
