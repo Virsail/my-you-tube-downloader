77b522a05af8830e5e51c2ac97a080a14695c657
virsail <virsaileric@gmail.com>
2020-08-09 22:24:01 +0300

    add the event listener

--- Server/index.js
+++ Server/index.js
@@ -5,3 +5,15 @@ const app = express();
 app.listen(4000, () => {
     console.log('Server Works !!! At port 4000');
 });
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+app.use(cors());
+app.listen(4000, () => {
+    console.log('Server Works !!! At port 4000');
+});
+app.get('/download', (req,res) => {
+    var URL = req.query.URL;
+    res.json({url:URL});
+})
